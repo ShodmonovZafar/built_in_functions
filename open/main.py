353d48200f47fe749fe66bf8e86d5a7d58b125b0
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2023-02-24 21:09:12 +0500

    z

--- open/main.py
+++ open/main.py
@@ -1,20 +1,21 @@
 import csv
+
+# path to the file
 path_csv_file = 'csv_files/csv_1.csv'
+path_text_file = "text_files/text.txt"
+path_text_file2 = "text_files/text2.txt"
+
+with open(path_text_file) as text_file:
+    pass
+
+with open(path_text_file2, "w") as text_file2:
+    pass
 
 with open(path_csv_file) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     print(csv_reader)
     for i in csv_reader:
         print(i)
-    # line_count = 0
-    # for row in csv_reader:
-    #     if line_count == 0:
-    #         print(f'Column names are {", ".join(row)}')
-    #         line_count += 1
-    #     else:
-    #         print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-    #         line_count += 1
-    # print(f'Processed {line_count} lines.')
-print(csv_reader)
-# for i in csv_reader:
-#     print(i)
+ 
+
+
